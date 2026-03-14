54596186a0c6bf8fb70ceb46e29207d696873776
Nandani Gohel <nandani.gohel@gmail.com>
2026-03-14 11:29:16 +0530

    feat(service): add ActivityLogService for logging task created, status changed, deleted and comment added events

---
+++ src/Service/ActivityLogService.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\ActivityLog;
+use App\Entity\Task;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ActivityLogService
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    public function logTaskCreated(Task $task, ?User $user = null): void
+    {
+        $log = new ActivityLog();
+        $log->setEventType('TASK_CREATED');
+        $log->setEventMessage(sprintf('Task "%s" was created', $task->getTitle()));
+        $log->setCreatedAt(new \DateTimeImmutable());
+        $log->setTask($task);
+        $log->setUser($user);
+
+        $this->entityManager->persist($log);
+        $this->entityManager->flush();
+    }
+
+    public function logStatusChanged(Task $task, ?User $user, string $oldStatus, string $newStatus): void
+    {
+        $log = new ActivityLog();
+        $log->setEventType('STATUS_CHANGED');
+        $log->setEventMessage(sprintf('Status changed from "%s" to "%s"', $oldStatus, $newStatus));
+        $log->setCreatedAt(new \DateTimeImmutable());
+        $log->setTask($task);
+        $log->setUser($user);
+
+        $this->entityManager->persist($log);
+        $this->entityManager->flush();
+    }
+
+    public function logTaskDeleted(Task $task, ?User $user = null): void
+    {
+        $log = new ActivityLog();
+        $log->setEventType('TASK_DELETED');
+        $log->setEventMessage(sprintf('Task "%s" was deleted', $task->getTitle()));
+        $log->setCreatedAt(new \DateTimeImmutable());
+        $log->setTask($task);
+        $log->setUser($user);
+
+        $this->entityManager->persist($log);
+        $this->entityManager->flush();
+    }
+
+    public function logCommentAdded(Task $task, ?User $user = null): void
+    {
+        $log = new ActivityLog();
+        $log->setEventType('COMMENT_ADDED');
+        $log->setEventMessage(sprintf('Comment added to task "%s"', $task->getTitle()));
+        $log->setCreatedAt(new \DateTimeImmutable());
+        $log->setTask($task);
+        $log->setUser($user);
+
+        $this->entityManager->persist($log);
+        $this->entityManager->flush();
+    }
+
+    public function getLogsForTask(Task $task): array
+    {
+        return $this->entityManager
+            ->getRepository(ActivityLog::class)
+            ->findBy(['task' => $task], ['createdAt' => 'DESC']);
+    }
+
+    /**
+     * For read-only view: only show the 3 meaningful event types
+     */
+    public function getReadOnlyLogsForTask(Task $task): array
+    {
+        return $this->entityManager
+            ->getRepository(ActivityLog::class)
+            ->createQueryBuilder('a')
+            ->where('a.task = :task')
+            ->andWhere('a.event_type IN (:types)')
+            ->setParameter('task', $task)
+            ->setParameter('types', ['TASK_CREATED', 'STATUS_CHANGED', 'TASK_DELETED'])
+            ->orderBy('a.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+}
